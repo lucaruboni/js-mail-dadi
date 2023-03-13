7a994de19af3c31684f8e50018a90e0c0d804614
lucaruboni <lucaruboni@gmail.com>
2023-03-13 14:47:02 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/* Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+Mail Bonus
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
+
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+
+Che ci sia un array da qualche parte?
+
+Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
